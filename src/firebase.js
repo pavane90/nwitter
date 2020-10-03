0bf8215775cd6fa46d08f2b630bd324c345d9dba
Takenotsuka <pavane90@gmail.com>
2020-10-03 09:18:12 +0900

    firebase

---
+++ src/firebase.js
@@ -0,0 +1,13 @@
+import * as firebase from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDSUjBhslxUC8lYzLYnEaT00x_nyRzmV6s",
+  authDomain: "nwitter-44612.firebaseapp.com",
+  databaseURL: "https://nwitter-44612.firebaseio.com",
+  projectId: "nwitter-44612",
+  storageBucket: "nwitter-44612.appspot.com",
+  messagingSenderId: "607790562860",
+  appId: "1:607790562860:web:87a6d560ae41eb140ddb89",
+};
+
+export default firebase.initializeApp(firebaseConfig);
